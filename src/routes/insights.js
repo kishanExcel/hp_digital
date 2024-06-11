b242fe98d8094efea1cc35eccf72f084452393fe
kishanExcel <kishan.excel2011@gmail.com>
2024-06-11 11:10:31 +0530

    completed

--- src/routes/insights.js
+++ src/routes/insights.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {getInsights } from '../controllers/insights.controller.js';
-
+import { getPageInsightsController } from '../controllers/insights.controller.js';
 
 const insightsRouter = express.Router();
 
 insightsRouter.get('/insights' , getInsights);
-
+insightsRouter.post('/insights', getPageInsightsController);
 
 export default insightsRouter;
+
